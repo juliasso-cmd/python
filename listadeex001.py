51fdd9c0e83dbba6f7f37277d2c3d4e85fe9d19b
Julio Lago <68673373+juliasso-cmd@users.noreply.github.com>
2024-01-10 07:33:55 -0300

    Create listadeex001.py

---
+++ listadeex001.py
@@ -0,0 +1,7 @@
+# Variáveis e Tipos de Dados:
+# Declare duas variáveis, uma com seu nome e outra com sua idade. Imprima na tela uma mensagem usando essas variáveis
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+print("meu nome é {} e eu tenho {} anos de idade.".format(nome,idade))
