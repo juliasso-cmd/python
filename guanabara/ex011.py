6731da4e973ba23c365b99fb112f3bf22a160db3
Julio Lago <68673373+juliasso-cmd@users.noreply.github.com>
2024-01-11 12:04:34 -0300

    update

--- guanabara/ex011.py
+++ guanabara/ex011.py
@@ -1,11 +1,9 @@
-# Faça um programa que leia a largura e a altura de uma parede em metros, calcule sua area e a quantidade de tinta necessária para pintá-la sabendo que cada litro de tinta pinta 2m
+# Faça um programa que leia a LARGURA e a ALTURA de uma parede em metros, calcule sua area e a quantidade de tinta necessária para pintá-la sabendo que cada litro de tinta pinta 2m
 
-larg=float(input("Digite a largura da parede: "))
-alt=float(input("Digite a altura da parede: "))
+larg = float(input("Digite a largura da parede: "))
+alt = float(input("Digite a altura da parede: "))
 
-area=larg*alt
-print("A dimensão da sua parede é {:g}x{:g} e tem {:g}m2\n".format(larg,alt,area))
+area = larg*alt
+litro = area/2
 
-tinta = area/2
-
-print("Para pintar essa parede você precisará de {:g}L de tinta\n".format(tinta))
+print("Sua parede tem {:g}x{:g} e precisará de {:g}L de tinta.".format(larg, alt, litro))
---
+++ listanet.py
@@ -0,0 +1,7 @@
+# Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit
+
+temp = float(input("Digite a temperatura que deseja converter em celcius: "))
+
+fahr = temp *1.8+32
+
+print("a temperatura em Fahrenheit é: {}".format(fahr)) 
