3fd928a977b3768d0d9e11ecba70cc3fcf191351
Julio Lago <68673373+juliasso-cmd@users.noreply.github.com>
2024-01-15 10:15:49 -0300

    ue

--- listanet.py
+++ listanet.py
@@ -1,7 +1,50 @@
-# Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit
+# # Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit
 
-temp = float(input("Digite a temperatura que deseja converter em celcius: "))
+# temp = float(input("Digite a temperatura que deseja converter em celcius: "))
 
-fahr = temp *1.8+32
+# fahr = temp *1.8+32
 
-print("a temperatura em Fahrenheit é: {}ºF".format(fahr)) 
+# print("a temperatura em Fahrenheit é: {}ºF".format(fahr)) 
+
+# -------------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------- #
+
+#Crie um programa que peça a idade do usuário e imprima se ele é maior de idade ou menor de idade
+
+# idade = int(input("Digite sua idade: "))
+
+# if idade >=18:
+#     print("Maior de idade!")
+# else:
+#     print("Menor de idade!")
+
+# -------------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------- #
+
+# imprima numeros de 0 a 10:
+
+# LOOPS
+# FOR:
+
+# WHILE:
+# cont = 0
+# while cont <=20:
+#     cont = cont + 1
+#     print(cont)
+
+# FUNÇOES COM LOOPS:
+# RANGE:
+# for i in range(10):
+#     print(i)
+    
+# RANGE W/ BREAK AND CONTINUE
+# for i in range(10):
+#     if i == 6:
+#         break
+#     print(i)
+
+
+# Crie um programa que verifica se um número é par ou ímpar
+
