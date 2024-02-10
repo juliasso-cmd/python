8bab5064a436c1f395338231ad5cb2fca601aaf2
Julio Lago <68673373+juliasso-cmd@users.noreply.github.com>
2024-02-10 14:00:25 -0300

    Update listadeex001.py

--- listadeex001.py
+++ listadeex001.py
@@ -1,7 +1,15 @@
-# Variáveis e Tipos de Dados:
-# Declare duas variáveis, uma com seu nome e outra com sua idade. Imprima na tela uma mensagem usando essas variáveis
+# # Variáveis e Tipos de Dados:
+# # Declare duas variáveis, uma com seu nome e outra com sua idade. Imprima na tela uma mensagem usando essas variáveis
 
-nome = input("Digite seu nome: ")
-idade = input("Digite sua idade: ")
+# nome = input("Digite seu nome: ")
+# idade = input("Digite sua idade: ")
 
-print("meu nome é {} e eu tenho {} anos de idade.".format(nome,idade))
+# print("meu nome é {} e eu tenho {} anos de idade.".format(nome,idade))
+
+
+
+from math import sqrt, ceil, floor
+
+n = int(input("Digite o numero: "))
+raiz = sqrt(n)
+print(raiz)
