36cb506c36e16b73dad6dcaf9ade6d77fc5fea65
Julio Lago <68673373+juliasso-cmd@users.noreply.github.com>
2024-02-20 11:41:31 -0300

    a

--- aula8/ex21a8.py
+++ aula8/ex21a8.py
@@ -1,3 +1,5 @@
 # Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 
 import pygame
+pygame.init()
+pygame.mixer
---
+++ aula9.py
@@ -0,0 +1,9 @@
+# Nessa aula, vamos aprender operações com uma cadeia de caracteres (String) no Python. As principais operações que vamos aprender são o Fatiamento de String, Análise com len(), count(), find(), transformações com replace(), upper(), lower(), capitalize(), title(), strip(), junção com join(). 
+
+frase = "Julio lindão"
+fatia = frase[:5]
+print(frase[9])
+print(frase[9:12])
+print (fatia)
+
+print(len(frase))
